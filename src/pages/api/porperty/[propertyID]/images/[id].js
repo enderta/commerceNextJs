b1dd59129d2a36464fca268ba68e64c619825adf
enderta <endertanver@gmail.com>
2023-05-01 22:35:00 +0100

    rental

---
+++ src/pages/api/porperty/[propertyID]/images/[id].js
@@ -0,0 +1,90 @@
+import {connectDb} from "@/pages/db";
+import {useRouter} from "next/router";
+
+// create dynamic routes for each method: /api/propertyID/1/images/1
+export default async function handler(req, res) {
+    const {client} = await connectDb();
+    const {method} = req;
+    const {propertyID, id} = req.query;
+
+    if (method === "GET") {
+        try {
+            const {rows} = await client.query(
+                'select * from property_images where property_id=$1 and id=$2',
+                [propertyID, id]
+            );
+            res.status(200).json({
+            status: "success",
+                message:`${rows.length} images found for property ${propertyID}`,
+                data: rows
+
+        });
+        } catch (error) {
+            res.status(500).json({
+                status: "error",
+                message: error.message,
+            });
+        }
+    } else if (method === "PUT") {
+        try {
+            const {rows} = await client.query(
+                'update property_images set image_url=$1 where property_id=$2 and id=$3 returning *',
+                [req.body.image_url, propertyID, id]
+            );
+            res.status(200).json({
+                status: "success",
+                message: `Image ${id} updated for property ${propertyID}`,
+                data: rows[0],
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "error",
+                message: error.message,
+            });
+        }
+    } else if (method === "DELETE") {
+        try {
+            const {rows} = await client.query(
+                'delete from property_images where property_id=$1 and id=$2',
+                [propertyID, id]
+            );
+            res.status(200).json({
+                status: "success",
+                message: `Image ${id} deleted for property ${propertyID}`,
+                data: rows[0],
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "error",
+                message: error.message,
+            });
+        }
+    }
+
+    else if(method==="DELETE"){
+        try {
+            const {rows} = await client.query(
+                'delete from property_images where property_id=$1 and id=$2',
+                [propertyID, id]
+            );
+            res.status(200).json({
+                status: "success",
+                message: `Image ${id} deleted for property ${propertyID}`,
+                data: rows[0],
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: "error",
+                message: error.message,
+            });
+        }
+    }
+    else {
+        res.status(400).json({
+            status: "error",
+            message: "Invalid request method",
+        });
+    }
+}
+
+
--- src/pages/api/rentalImages.js
+++ src/pages/api/rentalImages.js
@@ -3,47 +3,43 @@ import {connectDb} from '../db';
 export default async function rentalImages(req, res) {
     const {method} = req;
     const {client} = await connectDb();
-    const property_id = req.query.property_id || "";
-    if(property_id){
-        if (method === 'GET') {
-            try {
-                const {rows} = await client.query(
-                    "SELECT * from property_images WHERE property_id=$1;"
-                    , [property_id],
-                );
-                res.status(200).json({
-                        status: "success",
-                        message: `${rows.length} images found for property_id: ${property_id}`,
-                     //put url in an array
-                        data: {
-                            rows
-                        },
-                        image_url: rows.map((item) => item.image_url)
-                    }
-                );
-            } catch (err) {
-                console.error(err);
-            }
+    if (method === 'GET') {
+        try {
+            const {rows} = await client.query(
+                "SELECT * FROM property_images;"
+
+            );
+            res.status(200).json({
+                    status: "success",
+                    message: `${rows.length} images found`,
+                    data: {
+                        rows
+                    },
+                image_url: rows.map((row) => row.image_url)
+                }
+            );
+        } catch (err) {
+            console.error(err);
         }
-    }else{
-        if(method==="GET"){
-            try {
-                const {rows} = await client.query(
-                    "SELECT * from property_images;"
-                );
-                res.status(200).json({
-                        status: "success",
-                        message: `${rows.length} images found`,
-                        data: {
-                            rows
-                        },
-                        image_url: rows.map((item) => item.image_url)
+    } else if (method === "POST") {
+        const {property_id, image_url} = req.body;
+        try {
+            const {rows} = await client.query(
+                "INSERT INTO property_images (property_id, image_url) VALUES ($1, $2);"
+                , [property_id, image_url],
+            );
+            res.status(200).json({
+                    status: "success",
+                    message: `image added for property_id: ${property_id}`,
+                    data: {
+                        rows
                     }
-                );
-            }
-            catch (err) {
-                console.error(err);
-            }
+                }
+            );
+        }
+        catch (err) {
+            console.error(err);
         }
     }
+
 }
