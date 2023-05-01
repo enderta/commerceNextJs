e8a4e8f14c16d76a6be2d93389d9865bd71e68a7
enderta <endertanver@gmail.com>
2023-05-01 14:22:19 +0100

    rental

--- src/pages/Carosel.js
+++ src/pages/Carosel.js
@@ -14,10 +14,6 @@ export default function Carosel(props) {
                        style={{height: "400px"}}
                        alt="First slide"
                    />
-                   <Carousel.Caption>
-                       <h3>First slide label</h3>
-                       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                   </Carousel.Caption>
                </Carousel.Item>
                <Carousel.Item>
                    <img
@@ -27,10 +23,6 @@ export default function Carosel(props) {
                        alt="Second slide"
                    />
 
-                   <Carousel.Caption>
-                       <h3>Second slide label</h3>
-                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                   </Carousel.Caption>
                </Carousel.Item>
                <Carousel.Item>
                    <img
@@ -40,12 +32,6 @@ export default function Carosel(props) {
                        alt="Third slide"
                    />
 
-                   <Carousel.Caption>
-                       <h3>Third slide label</h3>
-                       <p>
-                           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                       </p>
-                   </Carousel.Caption>
                </Carousel.Item>
            </Carousel>
        </>
