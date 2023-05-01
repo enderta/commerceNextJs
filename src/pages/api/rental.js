c720c50be207e3057b1849e2041580a6e9f4200a
enderta <endertanver@gmail.com>
2023-05-01 14:30:31 +0100

    rental

--- src/pages/api/rental.js
+++ src/pages/api/rental.js
@@ -6,86 +6,81 @@ export default async function rental(req, res) {
     const {method} = req;
     const {client} = await connectDb();
     const search = req.query.search || "";
-   const is_rental = req.query.is_rental || "";
+    const is_rental = req.query.is_rental || "";
     if (method === 'GET') {
-        try{
+        try {
             //rental true and search
             if (search && is_rental) {
-    //select a.*,p.*,i.* from properties p join property_images i on p.id=i.property_id join address a on p.address_id=a.id ORDER BY P.price ASC;
+                //select a.*,p.*,i.* from properties p join property_images i on p.id=i.property_id join address a on p.address_id=a.id ORDER BY P.price ASC;
                 const {rows} = await client.query(
                     //search by city, zipcode, address,country,state
                     "SELECT a.*,p.* from properties p join address a on p.address_id=a.id WHERE (a.city ILIKE $1 OR a.zip_code ILIKE $2 OR a.address_line_1 ILIKE $3 OR a.country ILIKE $4 OR a.state ILIKE $5) AND p.is_rental=$6 ORDER BY p.price ASC;"
-                    ,[`%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`, is_rental],
-
+                    , [`%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`, is_rental],
                 );
                 res.status(200).json({
-                    status: "success",
-                    message: `${rows.length} properties found for search term: ${search} and rental: ${is_rental}`,
-                    data: {
-                        rows
+                        status: "success",
+                        message: `${rows.length} properties found for search term: ${search} and rental: ${is_rental}`,
+                        data: {
+                            rows
 
+                        }
                     }
-                }
                 );
 
 
-        }
-        //rental true and no search
-        else if(is_rental){
-            const {rows} = await client.query(
-                "SELECT a.*,p.* from properties p join address a on p.address_id=a.id WHERE p.is_rental=$1 ORDER BY p.price ASC;"
-                ,[is_rental],
-
-            );
-            res.status(200).json({
-                status: "success",
-                message: `${rows.length} properties found for rental: ${is_rental}`,
-                data: {
-                    rows
-
-                }
             }
-            );
-        }
-        //no rental and search
-        else if(search){
-            const {rows} = await client.query(
-                "SELECT a.*,p.* from properties p join address a on p.address_id=a.id WHERE (a.city ILIKE $1 OR a.zip_code ILIKE $2 OR a.address_line_1 ILIKE $3 OR a.country ILIKE $4 OR a.state ILIKE $5) ORDER BY p.price ASC;"
-                ,[`%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`],
+            //rental true and no search
+            else if (is_rental) {
+                const {rows} = await client.query(
+                    "SELECT a.*,p.* from properties p join address a on p.address_id=a.id WHERE p.is_rental=$1 ORDER BY p.price ASC;"
+                    , [is_rental],
+                );
+                res.status(200).json({
+                        status: "success",
+                        message: `${rows.length} properties found for rental: ${is_rental}`,
+                        data: {
+                            rows
 
-            );
-            res.status(200).json({
-                status: "success",
-                message: `${rows.length} properties found for search term: ${search}`,
-                data: {
-                    rows
-                }
+                        }
+                    }
+                );
             }
-            );
-        }
-        //no rental and no search
-        else{
-            const {rows} = await client.query(
-                "SELECT a.*,p.* from properties p join address a on p.address_id=a.id ORDER BY p.price ASC;"
-
-            );
-            res.status(200).json({
-                status: "success",
-                message: `${rows.length} properties found`,
-                data: {
-                    rows
-                }
+            //no rental and search
+            else if (search) {
+                const {rows} = await client.query(
+                    "SELECT a.*,p.* from properties p join address a on p.address_id=a.id WHERE (a.city ILIKE $1 OR a.zip_code ILIKE $2 OR a.address_line_1 ILIKE $3 OR a.country ILIKE $4 OR a.state ILIKE $5) ORDER BY p.price ASC;"
+                    , [`%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`, `%${search}%`],
+                );
+                res.status(200).json({
+                        status: "success",
+                        message: `${rows.length} properties found for search term: ${search}`,
+                        data: {
+                            rows
+                        }
+                    }
+                );
             }
-            );
+            //no rental and no search
+            else {
+                const {rows} = await client.query(
+                    "SELECT a.*,p.* from properties p join address a on p.address_id=a.id ORDER BY p.price ASC;"
+                );
+                res.status(200).json({
+                        status: "success",
+                        message: `${rows.length} properties found`,
+                        data: {
+                            rows
+                        }
+                    }
+                );
+            }
+
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        } finally {
+            client.release();
         }
-
-
-
-    }catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server error");
     }
-    finally {
-        client.release();
-    }
-}}
+}
