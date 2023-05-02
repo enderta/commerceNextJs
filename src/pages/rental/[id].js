6f32cb0088c4711276dd6490a108c752fdc810f8
enderta <endertanver@gmail.com>
2023-05-02 16:05:10 +0100

    rental

--- src/pages/rental/[id].js
+++ src/pages/rental/[id].js
@@ -1,9 +1,17 @@
 import {connectDb} from "@/pages/db";
 import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
 
 export default function home(){
     const router = useRouter();
-    const {id} = router.query;
+   const id= router.query.id;
+    const [property, setProperty] = useState({});
+
+    useEffect(
+        fetch(`/api/single/${id}`).then(res => res.json()).then(data => setProperty(data))
+
+    )
+
     return(
         <div>
             <h1>Details</h1>
