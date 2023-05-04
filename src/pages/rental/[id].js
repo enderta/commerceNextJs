87706fa15c54597f5d3b26b672c493a66a7c845a
enderta <endertanver@gmail.com>
2023-05-04 13:34:30 +0100

    rental

--- src/pages/rental/[id].js
+++ src/pages/rental/[id].js
@@ -1,33 +1,45 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import {Container, Row, Col, Image} from "react-bootstrap";
+
 
 export default function Home() {
     const router = useRouter();
-    const { id } = router.query;
+    const {id} = router.query;
     const [property, setProperty] = useState({});
+    const [darkMode, setDarkMode] = useState(false);
+
+
+    function handleToggle() {
+        setDarkMode(!darkMode);
+    }
 
     useEffect(() => {
         const getProperty = async () => {
             const response = await fetch(`http://localhost:3000/api/single/${id}`);
             const data = await response.json();
             setProperty(data.data[0]);
-        }
-        getProperty().then(r => console.log(r));
+        };
+        getProperty().then((r) => console.log(r));
     }, [id]);
 
     return (
-        <div>
-            <h1>Details</h1>
-            {property.id &&
-                <>
-                    <h2>{property.id}</h2>
-                    <p>{property.description}</p>
-                    <p>Price: {property.price} {property.currency}</p>
-                    <p>{property.is_rental ? 'For rent' : 'For sale'}</p>
-                    <img src={property.image_url} alt={property.title} />
-                </>
-            }
-        </div>
+
+
+            <div className={`${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'} min-h-screen`}>
+            <br/>
+            <div className="flex items-center justify-between">
+                <button
+                    type="button"
+                    onClick={handleToggle}
+                >
+                    {darkMode ? <span>&#x2600; </span> : <span>&#127769;</span>}
+                </button>
+            </div>
+            </div>
+
+
+
     );
 }
