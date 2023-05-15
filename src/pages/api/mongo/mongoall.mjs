092ba3cdc0551990989370619a41880096315eb0
enderta <endertanver@gmail.com>
2023-05-15 13:20:20 +0100

    mongo

--- src/pages/api/mongo/mongoall.mjs
+++ src/pages/api/mongo/mongoall.mjs
@@ -1,16 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
 import BlogPost from "./scheme.mjs";
 import connect from './monogo.mjs'
+
+
 const app = express();
 
 connect().then(r => console.log(r)).catch(e => console.log(e));
 
-let id=1;
 const insert = async (req, res) => {
     const { title, content, author } = req.body;
     const newPost = new BlogPost({
-        id: id++,
         title,
         content,
         author,
@@ -18,14 +17,8 @@ const insert = async (req, res) => {
     try {
         await newPost.save();
         res.status(201).json({
-            message: "Post created successfully.",
-            post: {
-                id: newPost.id,
-                title: newPost.title,
-                content: newPost.content,
-                author: newPost.author,
-                created_at: newPost.created_at,
-            }
+            message: "Post created successfully",
+            data: newPost,
         });
     } catch (error) {
         res.status(409).json({ message: error.message });
@@ -39,15 +32,8 @@ const getPosts = async (req, res) => {
     try {
         const posts = await BlogPost.find();
         res.status(200).json({
-            message:`${posts.length} posts found.`,
-            data:{
-                posts:posts.map((post)=>({
-                    id:post.id,
-                    title:post.title,
-                    content:post.content,
-                    created_at:post.created_at,
-                }))
-            }
+            message: `${posts.length} posts found`,
+            data: posts,
         });
     } catch (error) {
         res.status(404).json({ message: error.message });
@@ -56,75 +42,50 @@ const getPosts = async (req, res) => {
 
 app.get("/posts", getPosts);
 
-const updatePost = async (req, res) => {
-    const id=req.params.id;
-    const { title, content, author } = req.body;
-    try{
-        const updatedPost = await BlogPost.findOneAndUpdate({ id: id }, { title, content, author }, { new: true });
-        if (!updatedPost) {
-            return res.status(404).json({ message: "Post not found." });
-        }
+const getOne= async (req, res) => {
+    const { id } = req.params;
+    try {
+        const post = await BlogPost.findById(id);
         res.status(200).json({
-            message: "Post updated successfully.",
-            post: {
-                id: updatedPost.id,
-                title: updatedPost.title,
-                content: updatedPost.content,
-                created_at: updatedPost.created_at,
-                updated_at: updatedPost.updated_at,
-            }
+            message: "Post found",
+            data: post,
         });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(404).json({ message: error.message });
     }
 };
 
-const findOne=async (req,res)=>{
-    const id=req.params.id;
-    try{
-        const getOne=await BlogPost.findOne({id:id});
-        if(!getOne){
-            return res.status(404).json({message:"Post not found."});
-        }
+app.get("/posts/:id", getOne);
+
+const update=async (req,res)=>{
+    const {id}=req.params;
+    const {title,content,author}=req.body;
+    try {
+        const updatedPost=await BlogPost.findByIdAndUpdate(id,{title,content,author},{new:true});
         res.status(200).json({
-            message:"Post found.",
-            post: {
-                id: getOne.id,
-                title: getOne.title,
-                content: getOne.content,
-                created_at: getOne.created_at,
-            }
-        });
-    }catch(error){
-        res.status(500).json({message:error.message});
+            message:"Post updated",
+            data:updatedPost
+        })
+    } catch (error) {
+        res.status(404).json({message:error.message})
     }
 }
 
-app.get("/posts/:id",findOne);
+app.patch("/posts/:id",update);
 
-app.put("/posts/:id", updatePost);
-
-const deletePost = async (req, res) => {
-    const id=req.params.id;
+const del=async (req,res)=>{
+    const {id}=req.params;
     try {
-        const deletedPost = await BlogPost.findOneAndDelete({ id: id });
-        if (!deletedPost) {
-            return res.status(404).json({ message: "Post not found." });
-        }
+        await BlogPost.findByIdAndDelete(id);
         res.status(200).json({
-            message: "Post deleted successfully.",
-            post: {
-                id: deletedPost.id,
-                title: deletedPost.title,
-                content: deletedPost.content,
-            }
-        });
+            message:"Post deleted"
+        })
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(404).json({message:error.message})
     }
-};
+}
 
+app.delete("/posts/:id",del);
 
-app.delete("/posts/:id", deletePost);
 
 app.listen(5000, () => console.log("Server Running"));
--- src/pages/api/mongo/scheme.mjs
+++ src/pages/api/mongo/scheme.mjs
@@ -1,14 +1,25 @@
 import mongoose from 'mongoose';
 
 const BlogPostSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: String, required: true },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now },
-    id: { type: Number, required: false },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date
+    }
 });
 
-const BlogPost = mongoose.model("BlogPost", BlogPostSchema);
-
-export default BlogPost;
+export default mongoose.model('BlogPost', BlogPostSchema);
